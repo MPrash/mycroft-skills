9442327416ecc2b948464eb6f894e6514fc6a9b7
Prashant Mirchandani <prashant.mirchandani@e-microanalytics.com>
2019-10-07 18:31:13 +0530

    Refactoring and Mouse Scroll command

--- skill-mouse-automation/__init__.py
+++ skill-mouse-automation/__init__.py
@@ -11,35 +11,59 @@ class MouseAutomationSkill(MycroftSkill):
 
         
 
-    @intent_handler(IntentBuilder("MouseLocation").require("Location"))
+    @intent_handler(IntentBuilder("MouseLocation").require("Mouse_Location"))
     def handle_mouse_location_intent(self, message):
         mouse = Controller()
         (x,y) = mouse.position
         self.speak_dialog("mouse.location",data={"left_position": x,"top_position": y})
 
-    @intent_handler(IntentBuilder("MouseMoveUp").require("Move").require("MoveUp"))
-    def handle_mouse_up_intent(self, message):
+    @intent_handler(IntentBuilder("MouseMoveUp").require("Mouse_Move").require("Up"))
+    def handle_mouse_move_up_intent(self, message):
         mouse = Controller()
         mouse.move(0,-50)
         self.speak_dialog("mouse.move",data={"direction": "Up","move_pixels": 50})
 
-    @intent_handler(IntentBuilder("MouseMoveDown").require("Move").require("MoveDown"))
-    def handle_mouse_down_intent(self, message):
+    @intent_handler(IntentBuilder("MouseMoveDown").require("Mouse_Move").require("Down"))
+    def handle_mouse_move_down_intent(self, message):
         mouse = Controller()
         mouse.move(0,+50)
         self.speak_dialog("mouse.move",data={"direction": "Down","move_pixels": 50})
 
-    @intent_handler(IntentBuilder("MouseMoveLeft").require("Move").require("MoveLeft"))
-    def handle_mouse_left_intent(self, message):
+    @intent_handler(IntentBuilder("MouseMoveLeft").require("Mouse_Move").require("Left"))
+    def handle_mouse_move_left_intent(self, message):
         mouse = Controller()
         mouse.move(-50,0)
         self.speak_dialog("mouse.move",data={"direction": "Left","move_pixels": 50})
 
-    @intent_handler(IntentBuilder("MouseMoveRight").require("Move").require("MoveRight"))
-    def handle_mouse_right_intent(self, message):
+    @intent_handler(IntentBuilder("MouseMoveRight").require("Mouse_Move").require("Right"))
+    def handle_mouse_move_right_intent(self, message):
         mouse = Controller()
         mouse.move(+50,0)
         self.speak_dialog("mouse.move",data={"direction": "Right","move_pixels": 50})
+
+    @intent_handler(IntentBuilder("MouseScrollUp").require("Mouse_Scroll").require("Up"))
+    def handle_mouse_scroll_up_intent(self, message):
+        mouse = Controller()
+        mouse.scroll(0,10)
+        self.speak_dialog("mouse.scroll",data={"direction": "Up","scroll_steps": 10})
+
+    @intent_handler(IntentBuilder("MouseScrollDown").require("Mouse_Scroll").require("Down"))
+    def handle_mouse_scroll_down_intent(self, message):
+        mouse = Controller()
+        mouse.scroll(0,-10)
+        self.speak_dialog("mouse.scroll",data={"direction": "Down","scroll_steps": 10})
+
+    @intent_handler(IntentBuilder("MouseScrollLeft").require("Mouse_Scroll").require("Left"))
+    def handle_mouse_scroll_left_intent(self, message):
+        mouse = Controller()
+        mouse.scroll(-10,0)
+        self.speak_dialog("mouse.scroll",data={"direction": "Left","scroll_steps": 10})
+
+    @intent_handler(IntentBuilder("MouseScrollRight").require("Mouse_Scroll").require("Right"))
+    def handle_mouse_scroll_right_intent(self, message):
+        mouse = Controller()
+        mouse.scroll(10,0)
+        self.speak_dialog("mouse.scroll",data={"direction": "Right","scroll_steps": 10}) 
 
 def create_skill():
     return MouseAutomationSkill()
